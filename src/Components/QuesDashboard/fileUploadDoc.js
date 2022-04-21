0ef5911865df9ae2bbda5190788423c28b57cdd8
Ganesh Mane <ganesh.mane@atcs.com>
2022-04-21 15:44:48 +0530

    docUploadUI1

---
+++ src/Components/QuesDashboard/fileUploadDoc.js
@@ -0,0 +1,13 @@
+
+import Card from '@mui/material/Card';
+import classes  from './fileuploadDoc.module.css';
+const fileUploadDoc= ()=>{
+
+
+    return(
+        <div className={classes.card}>
+<Card variant="outlined">hiii</Card>
+        </div>
+    )
+}
+export default fileUploadDoc;
